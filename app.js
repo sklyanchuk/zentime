0f5bf9d11000153fd3506b1eb5e6c331551c0feb
Artem Sklyanchuk <artem.sklyanchuk@gmail.com>
2017-08-11 22:42:00 +0300

    Added React, Babel and some 'hello world'

---
+++ app.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+const styles = {
+  app: {
+    paddingTop: 40,
+    textAlign: 'center',
+    fontSize: '26px',
+  },
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div style={styles.app}>Welcome to Zen Time!</div>
+  )
+  }
+}
+
+const root = document.querySelector('#app')
+ReactDOM.render(<App />, root)
