53f30dc5667ad48681353d0d46400de4d72d9606
Artem Sklyanchuk <artem.sklyanchuk@gmail.com>
2017-08-16 21:45:11 +0300

    some optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,38 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const
+  webpack = require('webpack'),
+  path = require('path'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'app.js'),
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: 'bundle.js',
-    },
-    devServer: {
-      contentBase: path.join(__dirname, 'dist'),
-      port: 3000,
-    },
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin(),
-      new CleanWebpackPlugin(['dist']),
-      new HtmlWebpackPlugin({template: path.join(__dirname, 'src', 'index.html')}),
-    ],
-    module: {
-      rules: [
-        {
-          test: /.js$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                cacheDirectory: true,
-              },
+  entry: path.join(__dirname, 'src', 'app.js'),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({template: path.join(__dirname, 'src', 'index.html')}),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
             },
-          ],
-        },
-      ],
-    },
+          },
+        ],
+      },
+    ],
+  },
 };
