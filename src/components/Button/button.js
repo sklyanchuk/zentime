70433c60b6c9e1d7d2a3e88021b5efa646c4bc07
Artem Sklyanchuk <artem.sklyanchuk@gmail.com>
2017-08-19 19:38:02 +0300

    added alert on entry btn click

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import './style.css';
 
 class Button extends Component {
 
+  static
+    propTypes = {
+      href: PropTypes.string,
+      className: PropTypes.string,
+      onClick: PropTypes.func,
+    };
+
   render() {
     return (
       <a href={this.props.href}
-         className={`btn ${this.props.className}`}>{this.props.title}</a>
+         className={`btn ${this.props.className}`}
+         onClick={this.props.onClick}>{this.props.title}</a>
     );
   }
 
--- src/components/Welcome-page/welcome-page.js
+++ src/components/Welcome-page/welcome-page.js
@@ -3,16 +3,24 @@ import Button from '../Button/button.js'
 import './style.css';
 
 class WelcomePage extends Component {
+
+  onEntryClick() {
+    alert('Service is not yet ready!');
+  }
+
   render() {
     return (
       <div className="welcome-page-title">
         <p>Welcome to Zen Time!</p>
         <Button title="GitHub"
                 href="https://github.com/sklyanchuk/zentime"
-                className="btn-about" />
+                className="btn-about"
+        />
         <Button title="Entry"
                 href="#"
-                className="btn-entry" />
+                className="btn-entry"
+                onClick={() => this.onEntryClick()}
+        />
       </div>
     );
   }
