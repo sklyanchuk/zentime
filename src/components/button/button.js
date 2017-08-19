044b624bc7ad88317dea6e475c72f59304ab75a5
Artem Sklyanchuk <artem.sklyanchuk@gmail.com>
2017-08-19 15:11:30 +0300

    button as a component

---
+++ src/components/button/button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Button extends Component {
+
+  render() {
+    return <a href={this.props.href} className={this.props.className}>{this.props.title}</a>
+  }
+
+}
+
+export default Button;
--- src/components/welcome-page/welcome-page.js
+++ src/components/welcome-page/welcome-page.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from '../button/button.js'
 import './style.css';
 
 class WelcomePage extends Component {
@@ -6,8 +7,12 @@ class WelcomePage extends Component {
     return (
       <div className="welcome-page-title">
         <p>Welcome to Zen Time!</p>
-        <a href="https://github.com/sklyanchuk/zentime"
-           className="btn btn-about">About</a>
+        <Button title="GitHub"
+                href="https://github.com/sklyanchuk/zentime"
+                className="btn btn-about"/>
+        <Button title="Entry"
+                href="#"
+                className="btn btn-entry"/>
       </div>
     )
   }
