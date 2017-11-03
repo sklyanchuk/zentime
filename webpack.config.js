e2b712ec477677ebee7c1c053349342010824d52
Oleg SKLYANCHUK <sklyanchuk@gmail.com>
2017-11-03 17:13:28 +0100

    chore(webpack): devServer --open --historyApiFallback
     - Open browser automatically for faster development
     - Use history api
     - Remove unnecessary contentBase

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
     filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    open: true,
     port: 3000,
   },
   plugins: [
