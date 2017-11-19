60a3415b040ad1841f97d9f091ecf26fa8fddc91
Artem Sklyanchuk <artem.sklyanchuk@gmail.com>
2017-11-19 15:26:25 +0300

    Review comments fixes

--- src/store.js
+++ src/store.js
@@ -1,15 +1,12 @@
-import {createStore, applyMiddleware, compose} from 'redux'
-import createLogger from 'redux-logger'
-
+import {createStore, applyMiddleware, compose} from 'redux';
+import createLogger from 'redux-logger';
 import rootReducer from './reducers';
 
 const middlewares = [];
 
-if (process.env.NODE_ENV !== `production`) {
+if (process.env.NODE_ENV !== 'production') {
   const logger = createLogger();
   middlewares.push(logger);
 }
 
-const store = compose(applyMiddleware(...middlewares))(createStore)(rootReducer);
-
-export default store;
+export const store = compose(applyMiddleware(...middlewares))(createStore)(rootReducer);
